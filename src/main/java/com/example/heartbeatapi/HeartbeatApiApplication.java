2441f38862a981dc9fd927037947457a20e04ce0
SJ <serge.szynkarski@le-campus-numerique.fr>
2024-08-17 13:36:18 +0200

    first commit

---
+++ src/main/java/com/example/heartbeatapi/HeartbeatApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.heartbeatapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartbeatApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeartbeatApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/heartbeatapi/HeartbeatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.heartbeatapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeartbeatApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
